7d9fff10d8246377b000177a0939bf559c78193c
IANAHB0517 <fromtheedge0517@gmail.com>
2024-04-02 23:47:06 +0900

    postPost

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 /** author : string;
  * title : string;
  * content : string;
  * likeCount : nmber;
- * commnetCount : nmber;
+ * commentCount : nmber;
  */
 
 interface PostModel {
@@ -14,7 +21,7 @@ interface PostModel {
   title: string;
   content: string;
   likeCount: number;
-  commnetCount: number;
+  commentCount: number;
 }
 
 let posts: PostModel[] = [
@@ -24,7 +31,7 @@ let posts: PostModel[] = [
     title: '뉴진스 민지',
     content: '메이크업 고치고 있는 민지',
     likeCount: 1000000,
-    commnetCount: 99999,
+    commentCount: 99999,
   },
   {
     id: 2,
@@ -32,7 +39,7 @@ let posts: PostModel[] = [
     title: '뉴진스 해리',
     content: '노래 연습 해리',
     likeCount: 1000000,
-    commnetCount: 99999,
+    commentCount: 99999,
   },
   {
     id: 3,
@@ -40,7 +47,7 @@ let posts: PostModel[] = [
     title: '뉴진스 굥냐',
     content: '제일 이쁜 굥냐',
     likeCount: 1000000,
-    commnetCount: 99999,
+    commentCount: 99999,
   },
 ];
 
@@ -50,8 +57,16 @@ export class PostsController {
 
   // 1) Get /posts
   //    모든 posts를 다 가지고온다
+  @Get()
+  getPosts() {
+    return posts;
+  }
+  // 2) GET /posts/:id
+  //    id에 해당하는 postsfmf rkwudhsek
+  //    예를 들어 id=1일 경우 id가 1인 포스트를 가져온다.
+
   @Get(':id')
-  getPosts(@Param('id') id: String) {
+  getPost(@Param('id') id: String) {
     const post = posts.find((post) => post.id === +id);
 
     if (!post) {
@@ -60,16 +75,29 @@ export class PostsController {
 
     return post;
   }
-  // 2) GET /posts/:id
-  //    id에 해당하는 postsfmf rkwudhsek
-  //    예를 들어 id=1일 경우 id가 1인 포스트를 가져온다.
-  @Get()
-  getPost() {
-    return posts;
-  }
 
   // 3) POST /posts
   //    post를 생성한다.
+
+  @Post()
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+
+    return post;
+  }
 
   // 4) PUT /posts/:id
   //    id에 해당하는 POST를 변경한다.
