dd21a45ec8a7a7f6eb32f1ad821615e98258599b
IANAHB0517 <fromtheedge0517@gmail.com>
2024-04-24 04:24:31 +0900

    debuger setting

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,7 +2,6 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PostsModel } from './entities/posts.entity';
-import { UsersModel } from 'src/users/entities/users.entity';
 
 /** author : string;
  * title : string;
