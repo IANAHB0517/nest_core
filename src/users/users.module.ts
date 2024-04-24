bd73987e7af27375d0cf5c811e993ab0835bb88a
IANAHB0517 <fromtheedge0517@gmail.com>
2024-04-25 05:18:27 +0900

    authentication

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,6 +8,9 @@ import { UsersModel } from './entities/users.entity';
   imports: [TypeOrmModule.forFeature([UsersModel])],
 
   controllers: [UsersController],
+  // providers 해당 모듈 내에서만 사용 가능한 클래스.
   providers: [UsersService],
+  // 다른 모듈에 import되었을 때 사용 가능 하도록 하려면 exports에 클래스를 선언해주어야 한다.
+  exports: [UsersService],
 })
 export class UsersModule {}
