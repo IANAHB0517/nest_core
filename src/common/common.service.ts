81d33ce9fa86f3967c136b8b70a39897ac5dcc1b
IANAHB0517 <fromtheedge0517@gmail.com>
2024-05-09 23:29:50 +0900

    test

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { BasePaginationDto } from './dto/base-pagination.dto';
 import { BaseModel } from './entity/base.entity';
-import { FindManyOptions, Repository } from 'typeorm';
+import { FindManyOptions, FindOptionsOrder, FindOptionsWhere, Repository } from 'typeorm';
 
 @Injectable()
 export class CommonService {
@@ -10,5 +10,142 @@ export class CommonService {
     repository: Repository<T>,
     overrideFindOptions: FindManyOptions<T> = {},
     path: string,
+  ) {
+    if (dto.page) {
+      return this.pagePaginate(dto, repository, overrideFindOptions);
+    } else {
+      return this.cursorPaginate(dto, repository, overrideFindOptions, path);
+    }
+  }
+
+  private async pagePaginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
   ) {}
+
+  private async cursorPaginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+    path: string,
+  ) {
+    /**
+     * where__likeCOuint__more_than
+     *
+     * where__title__ilike
+     */
+  }
+
+  private composeFindOptions<T extends BaseModel>(
+    dto: BasePaginationDto,
+  ): FindManyOptions<T> {
+    /**
+     * where,
+     * order,
+     * take,
+     * skip -> page 기반일 때만
+     */
+    /** DTO의 현재 생긴 구조는 아래와 같다
+     *
+     *
+     * {
+     * where__id__more_than: 1,
+     * order__createdAt: 'ASC'
+     * }
+     *
+     * 현재 where__id__more_than / where__id__less_than에 해당되는 where 필터만 사용중이지만 나중에는 where__likeCount__more_than 이나 where__title__ilike 등 추가 필터를 넣고 싶어졌을때 모든 where 필터들을 자동으로 파싱 할 수 있을만한 기능을 제작해야한다.
+     *
+     *  1) where로 시작한다면 필터 로직을 적용한다.
+     *  2) order로 시작한다면 정렬 로직을 적용한다.
+     *  3) 필터 로직을 적용한다면 '__' 기준으로 split 했을때 3개의 값으로 나뉘는지 2개의 값으로 나뉘는지 확인한다.
+     *
+     * 3-1) 3개의 값으로 나뉜다면 FILTER_MAPPER에서 해당되는 operator 함수를 찾아서 적용한다.
+     *      ['where', 'id', 'more_than']
+     * 3-2) 2개의 값으로 나뉜다면 정확한 값을 필터하는 것이기 때문에 operator 없이 적용한다.
+     *      ['where', 'id']
+     *
+     *  4) order의 경우 3-2와 같이 적용한다.
+     */
+
+    let where: FindOptionsWhere<T> = {};
+
+    let order: FindOptionsOrder<T> = {};
+
+    for (const [key,value] of Object.entries(dto)){
+        // key -> where__id__less_than
+        // value -> 1
+
+        if(key.startsWith('where__')){
+            where = {
+                ...where,
+                ...this.parseWhereFilter(key, value)
+            }
+        } else if(key.startsWith('order__')){
+            order = {
+                ...order,
+                ...this.parseOrderFilter(key, value)
+            }
+        }
+
+        return {
+            where ,
+            order ,
+            take : dto.take,
+            skip : dto.page ? dto.take * (dto.page -1) : null,
+        };
+
+    };
+
+    private parseWhereFilter<T extends BaseModel> (key: string, value:any) :
+    FindManyOptions<T>{
+        const options : FindOptionsWhere<T> = {};
+
+        /**
+         * 예를 들어 where__id__more_than
+         * __를 기준으로 나눴을때
+         * 
+         * ['where', 'id', 'more_than']으로 나눌 수 있다.
+         */
+
+        const split = key.split('__');
+
+        if(split.length !== 2 && split.length !== 3){
+            throw new BadRequestException(`where 필터는 '__'로 split 했을때 길이가 2 또는 3이어야 합니다 - 문제 되는 키값 : ${key}`);
+        }  
+        
+        if (split.length === 3){
+            /**
+             * 길이가 3일 경우에는 typeORM 유틸리티가 적용이 필요한 경우다
+             * 
+             * where__id__more_than의 경우
+             * where는 버려도 되고 두번째 값을 필터할 키값이 되고
+             * 세번쨰 값은 typeORM 유틸리티가 된다.
+             * 
+             * FILTER_MAPPER에 미리 정의해둔 값
+             */
+ 
+        } else if (split.length === 2){
+            /**
+             * ['where', 'id']
+             */
+            const [_, field] = split;
+            /**
+             * feild -> 'id'
+             * value -> 3
+             * 
+             * {
+             *  id:3,
+             * }
+             */
+            options[field] = value;
+        }
+    };
+    private parseOrderFilter<T extends BaseModel> (key: string, value:any) :
+    FindManyOptions<T>{
+
+
+        
+    };
+
 }
