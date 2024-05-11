df3c2f8210a067119f556b206e9516f708e259c7
IANAHB0517 <fromtheedge0517@gmail.com>
2024-05-11 23:37:33 +0900

    filter_mapper_2

--- src/common/const/filter-mapper.const.ts
+++ src/common/const/filter-mapper.const.ts
@@ -16,6 +16,29 @@ import {
   Not,
 } from 'typeorm';
 
+/**
+ * where__id__not
+ *
+ * {
+ *  where:{
+ *    id:Not(value)
+ *  }
+ * }
+ */
+
 export const FILTER_MAPPER = {
   not: Not,
+  lees_than: LessThan,
+  less_than_or_equal: LessThanOrEqual,
+  more_than: MoreThan,
+  more_than_or_equal: MoreThanOrEqual,
+  i_liek: ILike,
+  is_null: IsNull,
+  in: In,
+  any: Any,
+  array_contained_by: ArrayContainedBy,
+  array_contains: ArrayContains,
+  array_overlap: ArrayOverlap,
+  between: Between,
+  equal: Equal,
 };
