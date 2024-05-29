55dbe83ee667d3112b80062c10f269d46f6f37ff
IANAHB0517 <fromtheedge0517@gmail.com>
2024-05-29 14:56:33 +0900

    fix NumberricIssue

--- src/posts/comments/dto/update-comments.dto.ts
+++ src/posts/comments/dto/update-comments.dto.ts
@@ -9,5 +9,7 @@ export class UpdateCommentDto extends PartialType(CommentsModel) {
   comment: string;
 
   @IsNumber()
+  // 옵셔널을 주지 않는 것만으로 넘버릭 오류가 발생했다 왜 인가??
+  @IsOptional()
   author: UsersModel;
 }
